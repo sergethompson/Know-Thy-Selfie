acf0b68cc0ccbfe5fb6ea95c7dbe319d6e4b910a
sergethompson <serge.thompson@gmail.com>
2014-02-26 15:44:47 -0500

    intitial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KnowThySelfie::Application.config.secret_key_base = '12a4f120a099b87db9d2f7d4012c15c346b89e1e20cf436b8745af824290c3ff3ac238b44b1245f1d3804ec32531f0e2f8a645b34c1fed66f80df1374f736a95'
