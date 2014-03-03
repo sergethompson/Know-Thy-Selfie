1ed3d30c4182800a82eacf83f6835dd07a6387ce
Scooterlicious <sfell2006@gmail.com>
2014-03-03 17:11:44 -0500

    Updated caption, moved model methods from controller into model

--- app/controllers/selfies_controller.rb
+++ app/controllers/selfies_controller.rb
@@ -1,7 +1,11 @@
+# Constants
 IMAGE_ANALYSIS = true
-#REKOGNIZE_JOBS = "face_detect_gender_race_emotion_face_recognize"
 REKOGNIZE_JOBS = "face_age_eye_closed_gender_race_emotion_face_recognize"
+TMP_FILE_PREFIX = "#{Rails.root}/public/tmp/Selfie_"
+TMP_FILE_SUFFIX = ".png"
 
+
+# The Selfies Controller for handing our actions
 class SelfiesController < ApplicationController
 
 
@@ -20,7 +24,7 @@ class SelfiesController < ApplicationController
 		chart1 = selfie_params["photobooth_image_data"].split(',')
 		image = Base64.decode64(chart1[1])
 
-		tmp_filename = "#{Rails.root}/public/tmp/Selfie_" + DateTime.now.strftime("%Y%m%dT%H%M%S")+".png"
+		tmp_filename = TMP_FILE_PREFIX + DateTime.now.strftime("%Y%m%dT%H%M%S") + TMP_FILE_SUFFIX
 
 		File.open(tmp_filename, 'wb') do|f|
 		  f.write(image)
@@ -31,10 +35,11 @@ class SelfiesController < ApplicationController
 
 		# This is intentional -- We can't perform the image analysis until the image is on S3
 		new_selfie.json_analysis = ""
-		#binding.pry
+
+		# Here's where the CarrierWave S3 magic happens.  When the new_selfie is saved, CarrierWave is activated and transports the object saved in image_url to S3.  This connection is made in the selfie.rb Model by declaring an uploader
 		new_selfie.save
 
-		#Now the image should be saved to S3.  At this point, we can send off the image information to our Rekognition api to grab our analysis
+		#Now the image should be saved to S3.  At this point, we can send off the image information to our Rekognition image analysis api to grab our analysis
 		client = Rekognize::Client::Base.new(api_key: ENV['REKOGNIZE_API_KEY'], api_secret: ENV['REKOGNIZE_API_SECRET'])
 	
 		ret_val = ""
@@ -46,17 +51,10 @@ class SelfiesController < ApplicationController
 		#Transform the hash into a JSON object for storage
 		new_selfie.json_analysis = JSON.generate(ret_val)
 
-		# this technique of setting the show_url from the ret_val sseems to work, but if the Rekognize doesn't work, we have no idea where our image went, so we'll keep this as show_url
-		#	new_selfie.show_url = ret_val["url"]
-
 		# The reason we need this data member is because we cannot access 'new_selfie.image_url.url' in JavaScript
 		new_selfie.show_url = new_selfie.image_url.url
 
-		#		new_selfie.caption = create_caption_from_json_analysis(new_selfie.json_analysis) 
-		#binding.pry
-
-		new_selfie.caption = create_caption_from_json_analysis(ret_val) 
-		set_confidence_values(new_selfie, ret_val)
+		Selfie.set_analysis_values(new_selfie, ret_val)
 
 		new_selfie.votes = 0
 	    new_selfie.latitude = 40.7403775 #TODO: grab from Photo if available
@@ -70,8 +68,6 @@ class SelfiesController < ApplicationController
 			format.html {}
 			format.json { render json: new_selfie}
 		end
-
-
 	end
 
 
@@ -83,135 +79,6 @@ class SelfiesController < ApplicationController
 			format.json {render json: @selfie}
 		end
 	end
-
-	# The image analysis returns a Ruby Hash with lots of values.  They are typically things like "Smile" and "confidence" (ie, 95% confidence the person is smiling).  It is NOT saying "This person is smiling 95% of their ability to smile".
-	def set_confidence_values(in_selfie, ret_val)
-		#ERROR
-		if (ret_val.has_key?("face_detection") == false)
-			#We have a problem houston!  No face detection happened!
-			return "No face detection key: No confidence values to set! #{__FILE__} : #{__LINE__}"
-		end
-
-		if (ret_val["face_detection"].length < 1)
-			#We have a problem...  There WAS a face detected but there's no pose
-			return "face_detection array is length 0.  Unable to set confidence values. #{__FILE__} : #{__LINE__}"
-		end
-
-		if (ret_val["face_detection"].length > 1)
-			#We have a problem...  There WAS a face detected but there's no pose
-			return "face_detection array is length: #{in_selfie["face_detection"].length}.  But we're only setting confidence values for [0] #{__FILE__} : #{__LINE__}"
-		end
-
-		face = ret_val["face_detection"][0]
-
-		in_selfie.age 			= face["age"]
-		in_selfie.race_string 	= face["race"].keys[0]
-		in_selfie.race_conf 	= face["race"][in_selfie.race_string]
-
-		if (face["emotion"].has_key?("confused")) 
-			in_selfie.confused	= face["emotion"]["confused"]
-		end
-		if (face["emotion"].has_key?("calm")) 	  
-			in_selfie.confused	= face["emotion"]["calm"]
-		end
-		if (face["emotion"].has_key?("angry") )	  
-			in_selfie.angry		= face["emotion"]["angry"]
-		end
-		if (face["emotion"].has_key?("happy") )	  
-			in_selfie.angry		= face["emotion"]["happy"]
-		end
-		if (face["emotion"].has_key?("sad") )	  
-			in_selfie.angry		= face["emotion"]["sad"]
-		end
-		if (face["emotion"].has_key?("surprised") )	  
-			in_selfie.angry		= face["emotion"]["surprised"]
-		end
-
-		in_selfie.roll 		= face["pose"]["roll"]
-		in_selfie.yaw  		= face["pose"]["yaw"]
-		in_selfie.pitch 	= face["pose"]["pitch"]
-
-		in_selfie.smile 	= face["smile"]
-		in_selfie.glasses 	= face["glasses"]
-		in_selfie.eye_closed 	= face["eye_closed"]
-		in_selfie.sex		= face["sex"]
-
-	end
-
-
-
-
-	def create_caption_from_json_analysis(json_analysis) 
-		ret_string = ""
-		#ERROR
-		if (json_analysis.has_key?("face_detection") == false)
-			#We have a problem houston!  No face detection happened!
-			return "Did not recognize a face #{__FILE__} : #{__LINE__}"
-		end
-
-		if (json_analysis["face_detection"].length < 1)
-			#We have a problem...  There WAS a face detected but there's no pose
-			return "Did not recognize a face #{__FILE__} : #{__LINE__}"
-		end
-
-		face = json_analysis["face_detection"][0]
-
-		if (face["age"] > 26)
-			ret_string = "New York selfies average a bit older"
-		elsif (face["age"] < 23.3)
-			ret_string = "The selfies in Sao Paulo tend to skew very young"
-		end
-
-		if (face["pose"]["roll"].abs > 8) #Head tilted to the side
-			ret_string = "Nice tilt!  Are you from Sao Paulo?"
-		elsif (face["pose"]["pitch"] > 8) #Looking DOWN
-			ret_string = "Is it raining?"
-		elsif (face["pose"]["pitch"] < -8) #Looking UP
-			ret_string = "Japanese selfies tend to look down"
-		end
-
-
-		if (face["emotion"].has_key?("happy"))		
-			if (face["emotion"]["happy"] > 50)
-				ret_string = "Bangkok has the most smiles" #Like you, few Moscovites smile in their Selfies
-			end
-		end
-
-		if (face["emotion"].has_key?("confused"))
-			if (face["emotion"]["confused"] > 50)
-				ret_string = "Do you have any questions?"
-			end
-		end
-
-		if (face["emotion"].has_key?("sad"))
-			if (face["emotion"]["sad"] > 50)  
-				ret_string = "Let’s talk about it :-("
-			end
-		end
-
-		if (face["emotion"].has_key?("angry"))
-			if (face["emotion"]["angry"] > 50)  
-				ret_string = "So this guy walks into a bar.  Ouch!"
-			end
-		end
-
-		if (face["smile"] > 81)
-			ret_string = "Nice smile!  Bangkok has the most smiles"
-		end
-
-
-		if (ret_string.length == 0)
-			if (face["sex"].abs < 20)
-				ret_string = "Foxy lady!"
-			elsif (face["sex"].abs > 80)
-				ret_string = "Hey Dude!"
-			else
-				ret_string = "Sorry, facial recognition failed to report anything interesting about this photo"
-			end
-		end
-		return ret_string		
-	end
-
 
 
 private
