88330d4e7e6f6907bc4b66e088509a575f7475ae
bmwertman <brad.wertman@leanmeanz.com>
2014-03-04 08:52:52 -0500

    polaroid camera view still in need of some tweaking

--- app/assets/javascripts/website/js/script.js
+++ app/assets/javascripts/website/js/script.js
@@ -1,6 +1,21 @@
 $(function(){
 	$( '#example' ).photobooth().on( "image", function( event, dataUrl ){
-		$( "#gallery" ).show().html( '<img src="' + dataUrl + '" >');
+		$(".photobooth canvas, .blind, ul").css("display", "none")
+		//     $('#flash').css('background-color','white');
+		//     setTimeout(function() {
+		//     $('#flash').css('background-color','')
+		// }, 50);
+		//     setTimeout(function() {
+		//         $('#flash').css('background-color','white')}, 125);
+		//     setTimeout(function() {
+		//     $('#flash').css('background-color','')
+		// }, 175);
+		$("#gallery").append('<div class="selfie"></div>')
+		$('.selfie').html( '<img src="' + dataUrl + '" >')
+		$( "#gallery" ).show();
+		$("#gallery" ).animate({ top: "+=175px" }, 4000);
+		$( "#pic-fade" ).animate({ top: "+=175px" }, 4000);
+    $( "#pic-fade").delay(2000).fadeTo(3000, 0);
 	});
 
 	/**
